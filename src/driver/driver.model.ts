fae5409e858832366173e9dbb73817ed9138ef0c
Michael Stepanov <michael@stepanoff.org>
2021-10-10 16:05:11 +0300

    Added missing field "note"

--- src/driver/driver.model.ts
+++ src/driver/driver.model.ts
@@ -8,23 +8,29 @@ import {
 } from 'typeorm';
 
 @ObjectType()
-@Entity()
+@Entity({ name: 'drivers' })
 export class DriverModel {
   @Field()
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
   id: string;
+
   @Field()
   @Column({ length: 255, nullable: false })
   name: string;
+
   @Field()
   @Column({ length: 50, nullable: true })
   phone: string;
+
   @Field()
   @Column({ length: 255, nullable: true })
+  note: string;
+
   @Field()
   @Column()
   @CreateDateColumn()
   created_at: Date;
+
   @Field()
   @Column()
   @UpdateDateColumn()
